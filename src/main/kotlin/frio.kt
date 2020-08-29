4017e39d087f3e2cc8b876d026731fd754f11bbd
Lanfig <orlandoaf@gmail.com>
2020-08-29 16:21:15 -0300

    Corrigido erro grotesco na entrada de dados de frio.kt; antes, getLine estava sendo usado para ler cada valor na 2a linha

--- src/main/kotlin/frio.kt
+++ src/main/kotlin/frio.kt
@@ -3,9 +3,12 @@ fun frio(vararg temps: Long): Int = temps.filter { it < 0 }.size
 
 // para submissão no Kattis
 fun main() {
-    val n = readLine()?.toInt() ?: 0
-    val result: LongArray = LongArray(n)
-    for (i in 0 until n)
-        result[i] = readLine()?.toLong() ?: 1000
-    println(frio(*result))
+    readLine()
+    val dados = readLine()
+
+    val result: LongArray
+    if (dados != null) {
+        result = dados.split(' ').map { it.toLong() }.toLongArray()
+        println(frio(*result))
+    }
 }
