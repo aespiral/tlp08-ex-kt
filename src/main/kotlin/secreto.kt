1e26895adffda642b8997861f1a7a12ef46eb807
Lanfig <orlandoaf@gmail.com>
2020-08-29 21:57:42 -0300

    Segredo funcional

--- src/main/kotlin/secreto.kt
+++ src/main/kotlin/secreto.kt
@@ -1,51 +1,74 @@
+/*
+ * Uma versão mais `funcional`, seguindo Richard Bird em `Thinking functionally with Haskell`
+ *
+ * Bird refuta o uso de índices (como em a[i]), pois eles dificultam composição, ou uma visão
+ * do problema que favorece à composição.
+ *
+ * Vamos começar com a visualização das estapas intermediárias no fluxo do programa,
+ * tendo como exemplo a string "helloFriend", que, tendo 11 letras, cabe em 4x4
+ *
+ * 1     "helloFriend"
+ * 2     [h e l l o F r i e n d * * * * *]
+ * 3   [ [h e l l] [o F r i] [e n d *] [* * * *] ]
+ * 3.1 [ [h] [e] [l] [l] ]
+ * 3.2 [ [h o] [e F] [l r] [l i] ]
+ * 3.3 [ [h o e] [e F n] [l r d] [l i *] ]
+ * 3.4 [ [h o e *] [e F n *] [l r d *] [l i * *] ]
+ * 4   [ [* e o h] [* n F e] [* d r l] [* * i l] ]
+ * 5   [ * e o h * n F e * d r l * * i l ]
+ * 6   [ e o h n F e d r l i l ]
+ * 7    "eohnFedrlil"
+ *
+ * Cada transição é uma função específica.
+ *
+ * 1     "helloFriend"
+ * A String vira uma lista do tamanho desejado e é completada com '*'
+ * str.toList().plus( List( tam*tam - str.length , { '*' })
+ * 2     [h e l l o F r i e n d * * * * *]
+ * A lista é segmentada listas menores de tamanho fixo tam
+ *  lista.chunked(4)
+ * 3   [ [h e l l] [o F r i] [e n d *] [* * * *] ]
+ *  VER ABAIXO
+ * 4   [ [* e o h] [* n F e] [* d r l] [* * i l] ]
+ * lista-de-listas.flatten()
+ * 5   [ * e o h * n F e * d r l * * i l ]
+ * lista.filter { it != '*' }
+ * 6   [ e o h n F e d r l i l ]
+ * lista.let {String(it.toCharArray())
+ * 7    "eohnFedrlil"
+ *
+ * DETALHAMENTO DO PASSO 3 -> 4
+ * 3   [ [h e l l] [o F r i] [e n d *] [* * * *] ]
+ * acc = [ [] [] [] [] ] lstnha= [h e l l] zip= [([],h) ([],e) ([],l) ([],l)]
+ * 3.1 [ [h] [e] [l] [l] ]
+ * acc = [ [h] [e] [l] [l] ] lstnha= [o F r i] zip= [([h],o) ([e],F) ([l],r) ([l],i)]
+ * 3.2 [ [h o] [e F] [l r] [l i] ]
+ * acc = [ [h o] [e F] [l r] [l i] ] lstnha= [e n d *] zip= [([h o],e) ([e F],n) ([l r],d) ([l i],*)]
+ * 3.3 [ [h o e] [e F n] [l r d] [l i *] ]
+ * acc = [ [h o e] [e F n] [l r d] [l i *] ] lstnha= [* * * *] zip= [([h o e],*) ([e F n],*) ([l r d],*) ([l i *],*) ]
+ * 3.4 [ [h o e *] [e F n *] [l r d *] [l i * *] ]
+ * ll.map( it.reversed() )
+ * 4   [ [* e o h] [* n F e] [* d r l] [* * i l] ]
+ *
+ */
 import kotlin.math.ceil
-import kotlin.math.roundToInt
 import kotlin.math.sqrt
 
-class Matriz(val msg: String) {
+fun segredo(msg: String): String {
     val tam: Int = msg.length.toDouble().let { ceil(sqrt(it)) }.toInt()
-    var dados: Array<CharArray> = Array(tam) { CharArray(tam) }
-
-    // ajuda no REPL; visualização das matrizes
-    override fun toString(): String {
-        var res:StringBuilder = StringBuilder(tam*tam + tam + 1)
-        res.append('\n')
-        for (i in 0 until tam) {
-            for (j in 0 until tam)
-                res.append(dados[i][j])
-            res.append('\n')
-        }
-        return String(res)
-    }
-
-    init {
-        var cursor = 0
-        for (i in 0 until tam)
-            for (j in 0 until tam) {
-                dados[i][j] = msg.getOrElse(cursor, { '*' })
-                cursor = cursor + 1
-            }
-    }
-
-    fun gira(): Matriz {
-        val res = Matriz(msg)
-        for (i in 0 until tam)
-            for (j in 0 until tam)
-                res.dados[i][j] = dados[tam-1 - j][i]
-        return res
-    }
-
-    fun extrai(): String {
-        var res:StringBuilder = StringBuilder(msg.length)
-        for (i in 0 until tam)
-            for (j in 0 until tam)
-                if (dados[i][j] != '*')
-                    res.append(dados[i][j])
-        return String(res)
-    }
+    val llvazia: List<List<Char>> = List(tam) { mutableListOf() }
+    fun f(acc: List<List<Char>>, lstnha: List<Char>): List<List<Char>> =
+            acc.zip(lstnha).map { (l, c) -> l.plus(listOf(c))}
+    return msg
+            .toList()
+            .plus( List(tam*tam - msg.length, { '*' } ) )
+            .chunked(tam)
+            .fold(llvazia, ::f)
+            .map { it.reversed() }
+            .flatten()
+            .filter { it != '*' }
+            .let { String(it.toCharArray())}
 }
-
-fun segredo(msg: String): String = Matriz(msg).gira().extrai()
 
 fun main() {
     val n: Int = readLine()?.toInt()?:0
